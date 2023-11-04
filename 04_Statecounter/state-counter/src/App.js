5eddbb28bd7139ca35b517925b6f8f4274ff99bf
PaolaVlsc <paolavlsc170698@gmail.com>
2023-11-04 15:26:44 +0200

    Completed

--- 04_Statecounter/state-counter/src/App.js
+++ 04_Statecounter/state-counter/src/App.js
@@ -1,8 +1,50 @@
 import "./index.css";
+import {useState} from "react";
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
+
+    const date = new Date("November 04 2023");
+    date.setDate(date.getDate() + count);
+
+
+    return (
+        <div>
+
+            <div>
+                <button onClick={() => setStep((count) => count - 1)}>-</button>
+                <span>Step: {step}</span>
+                <button onClick={() => setStep((c) => c + 1)}>+</button>
+            </div>
+
+            <div>
+                <button onClick={() => setCount((c) => c - step)}>-</button>
+                <span>Count: {count}</span>
+                <button onClick={() => setCount((c) => c + step)}>+</button>
+            </div>
+
+            <p>
+                <span>
+                    {/* Conditional rendering*/}
+                    {count === 0 ?
+                        "Today is "
+                        : count > 0 ?
+                            `${count} days from today is `
+                            : `${Math.abs(count)} days ago was:`}
+
+                </span>
+                <span>{date.toDateString()}</span>
+            </p>
+        </div>
+    );
+}
 
 export default function App() {
     return (
         <div className="App">
+            <Counter/>
         </div>
     );
 }
