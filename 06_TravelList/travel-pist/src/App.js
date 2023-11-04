9cf401075a3f28475040ace3a332a6c1c3d39a12
PaolaVlsc <paolavlsc170698@gmail.com>
2023-11-04 20:31:45 +0200

    Stats - footer updated

--- 06_TravelList/travel-pist/src/App.js
+++ 06_TravelList/travel-pist/src/App.js
@@ -30,17 +30,25 @@ const Logo = () => {
 
 // TODO 3: Form Component
 const Form = () => {
-
+    return (
+        <div className={"add-form"}></div>
+    );
 }
 
 
 // TODO 4: Packing List Component
 const PackingList = () => {
-
+return(
+    <div className={"list"}></div>
+);
 }
 
 // TODO 5: Stats
 const Stats = () => {
-
+    return (
+        <footer className={"stats"}>
+            <em>You have X items on your list, and you've already packed X (X%)</em>
+        </footer>
+    );
 }
 export default App;
