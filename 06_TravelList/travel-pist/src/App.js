58eedb8b683d368e7a999b3a3220145391fa0138
PaolaVlsc <paolavlsc170698@gmail.com>
2023-11-04 20:46:48 +0200

    Form semantic html

--- 06_TravelList/travel-pist/src/App.js
+++ 06_TravelList/travel-pist/src/App.js
@@ -32,6 +32,13 @@ const Form = () => {
     return (
         <form className={"add-form"}>
             <h3>What do you need for your trip?</h3>
+            <select>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+            </select>
+            <input type={"text"} placeholder={"Item..."} />
+            <button>Add</button>
         </form>
     );
 }
