15573f4c4e74a1c5629a2e6af004b41335e2dc2d
PaolaVlsc <paolavlsc170698@gmail.com>
2023-11-04 20:42:13 +0200

    Add static item

--- 06_TravelList/travel-pist/src/App.js
+++ 06_TravelList/travel-pist/src/App.js
@@ -1,58 +1,66 @@
 // data
-const initialItems = [
-    {id: 1, description: "Passports", quantity: 2, packed: false},
-    {id: 2, description: "Socks", quantity: 12, packed: false},
-    {id: 3, description: "Charger", quantity: 1, packed: true},
-];
+import * as PropTypes from "prop-types";
+
+const initialItems = [{id: 1, description: "Passports", quantity: 2, packed: false}, {
+    id: 2,
+    description: "Socks",
+    quantity: 12,
+    packed: false
+}, {id: 3, description: "Charger", quantity: 1, packed: true},];
 
 
 // TODO 1: App Component
 const App = () => {
 
-    return (
-        <div className={"app"}>
-            <Logo/>
-            <Form/>
-            <PackingList/>
-            <Stats/>
-        </div>
-    );
+    return (<div className={"app"}>
+        <Logo/>
+        <Form/>
+        <PackingList/>
+        <Stats/>
+    </div>);
 }
 
 
 // TODO 2: Logo Component
 const Logo = () => {
-    return (
-        <h1>🌴 Far Away 🌴</h1>
-    );
+    return (<h1>🌴 Far Away 🌴</h1>);
 }
 
 
 // TODO 3: Form Component
 const Form = () => {
-    return (
-        <div className={"add-form"}>
-            <h3>What do you need for your trip?</h3>
-        </div>
-    );
+    return (<div className={"add-form"}>
+        <h3>What do you need for your trip?</h3>
+    </div>);
 }
 
+// TODO 4.02: Item Component in the list
+function Item({item}) {
+    return (
+        <li>
+            <span>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>);
+}
 
+Item.propTypes = {item: PropTypes.any};
 // TODO 4: Packing List Component
 const PackingList = () => {
-    return (
-        <ul className={"list"}>
+    return (<div className={"list"}>
+        <ul>
 
+            {/*TODO 04.01: Render List*/}
+            {initialItems.map(item => <Item item={item}/>)}
         </ul>
-    );
+    </div>);
 }
 
 // TODO 5: Stats
 const Stats = () => {
-    return (
-        <footer className={"stats"}>
-            <em>You have X items on your list, and you've already packed X (X%)</em>
-        </footer>
-    );
+    return (<footer className={"stats"}>
+        <em>You have X items on your list, and you've already packed X (X%)</em>
+    </footer>);
 }
 export default App;
