252c08dc0e04e937250312e605b895efc674cea8
PaolaVlsc <paolavlsc170698@gmail.com>
2023-11-04 20:25:47 +0200

    Logo updated

--- 06_TravelList/travel-pist/src/App.js
+++ 06_TravelList/travel-pist/src/App.js
@@ -1,36 +1,46 @@
+// data
+const initialItems = [
+    {id: 1, description: "Passports", quantity: 2, packed: false},
+    {id: 2, description: "Socks", quantity: 12, packed: false},
+    {id: 3, description: "Charger", quantity: 1, packed: true},
+];
+
+
 // TODO 1: App Component
 const App = () => {
 
     return (
         <div className={"app"}>
-            <Logo />
-            <Form />
-            <PackingList />
-            <Stats />
+            <Logo/>
+            <Form/>
+            <PackingList/>
+            <Stats/>
         </div>
     );
 }
 
 
 // TODO 2: Logo Component
-const Logo =  () => {
-
+const Logo = () => {
+    return (
+        <h1>🌴 Far Away 🌴</h1>
+    );
 }
 
 
 // TODO 3: Form Component
-const Form =  () => {
+const Form = () => {
 
 }
 
 
 // TODO 4: Packing List Component
-const PackingList =  () => {
+const PackingList = () => {
 
 }
 
 // TODO 5: Stats
-const Stats =  () => {
+const Stats = () => {
 
 }
 export default App;
