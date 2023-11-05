93d0fe5d678d524f0781d4e4633752cfe8dab67c
PaolaVlsc <paolavlsc170698@gmail.com>
2023-11-05 12:24:01 +0200

    Sort By functionability

--- 06_TravelList/travel-pist/src/App.js
+++ 06_TravelList/travel-pist/src/App.js
@@ -125,19 +125,37 @@ function Item({item, onDeleteItem, onToggleItem}) {
 
 // TODO 4: Packing List Component
 const PackingList = ({items, onDeleteItem, onToggleItem}) => {
+
+    // state
+    const [sortBy, setSortBy] = useState("input");
+
+    // derive state
+    let sortedItems;
+
+    // 1st option
+    if (sortBy === "input")
+        sortedItems = items;
+
+    // 2nd option
+    if (sortBy === "description")
+        sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description)); // sort alphabetically using localeCompare
+
+    // 3rd option
+    if (sortBy === "packed")
+        sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
     return (
         <div className={"list"}>
             <ul>
                 {/*TODO 04.01: Render List*/}
-                {items.map(item => <Item item={item} key={item.id} onDeleteItem={onDeleteItem}
+                {sortedItems.map(item => <Item item={item} key={item.id} onDeleteItem={onDeleteItem}
                                          onToggleItem={onToggleItem}/>)}
             </ul>
 
             <div className={"actions"}>
-                <select>
+                <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
                     <option value={"input"}> Sort by input order</option>
                     <option value={"description"}> Sort by description</option>
-                    <option value={"packed"}>Sort by packed item</option>
+                    <option value={"packed"}>Sort by packed status</option>
                 </select>
             </div>
         </div>
