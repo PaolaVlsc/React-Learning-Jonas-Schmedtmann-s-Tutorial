ff521c48219e9aa84ad5cb58cdfe16d5b8dab42b
PaolaVlsc <paolavlsc170698@gmail.com>
2023-11-05 11:06:58 +0200

    Add Checkbox

--- 06_TravelList/travel-pist/src/App.js
+++ 06_TravelList/travel-pist/src/App.js
@@ -109,6 +109,10 @@ const Form = ({onAddItems}) => {
 function Item({item, onDeleteItem}) {
     return (
         <li>
+            <input type={"checkbox"} value={item.packed} onChange={() => {
+
+            }}/>
+
             {/*Conditionally styling*/}
             <span style={item.packed ? {textDecoration: "line-through"} : {}}>
                 {item.quantity} {item.description}
