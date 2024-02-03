c95be10e0164d1c03423b73ea93cf3cc35a73e8c
PaolaVlsc <paolavlsc170698@gmail.com>
2024-02-03 11:23:42 +0200

    Accordion example using react ver01

---
+++ 07_Accordion/accordion-react/src/component/Accordion.js
@@ -0,0 +1,42 @@
+import {useState} from "react";
+
+
+// Accordion as in the whole component
+const Accordion = ({data}) => {
+    return (
+        <div className={"accordion"}>
+
+            {data.map((item, index) =>
+                (<AccordionItem title={item.title} text={item.text} num={index} key={item.title}/>))
+            }
+        </div>
+    );
+}
+
+// Accordion's items
+function AccordionItem({num, title, key, text}) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function handleToggle() {
+        setIsOpen(isOpen => !isOpen);
+    }
+
+    return (
+        <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+
+            {/*this line ensures that the item number is displayed with a leading zero
+            if it's a single-digit number (less than 9) to maintain a consistent two-digit format in the UI.*/}
+            <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+            <p className="title">{title}</p>
+            <p className="icon">{isOpen ? "-" : "+"}</p>
+
+            {/*expand */}
+
+            {isOpen && <div className={"content-box"}>{text}</div>}
+        </div>
+    );
+}
+
+
+export default Accordion;
