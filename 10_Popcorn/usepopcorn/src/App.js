6c9ff4fd5f44a480e0eaf519d57476afd3480d0e
PaolaVlsc <paolavlsc170698@gmail.com>
2024-02-04 15:21:06 +0200

    composition component

--- 10_Popcorn/usepopcorn/src/App.js
+++ 10_Popcorn/usepopcorn/src/App.js
@@ -55,11 +55,16 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies}>
+      <NavBar>
         <Search />
         <NumResults movies={movies} />
       </NavBar>
-      <Main movies={movies} />
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
@@ -102,16 +107,11 @@ function NumResults({ movies }) {
     </p>
   );
 }
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -121,7 +121,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
