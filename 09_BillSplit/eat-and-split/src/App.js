33b96d22981ef1bc3155b5b675b76071feedcc4e
PaolaVlsc <paolavlsc170698@gmail.com>
2024-02-07 23:51:09 +0200

    u[dates on package json settings and the logic of which friend owes me

--- 09_BillSplit/eat-and-split/src/App.js
+++ 09_BillSplit/eat-and-split/src/App.js
@@ -53,9 +53,26 @@ const FriendsList = () => {
 };
 
 const Friend = ({ friend }) => {
-  return;
-  <li key={friend.id}>
-    <img src={friend.image} alt={friend.name} />
-    <h3>{friend.name}</h3>
-  </li>;
+  return (
+    <li key={friend.id}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {/* implement the logic of the friend balance here  */}
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 };
