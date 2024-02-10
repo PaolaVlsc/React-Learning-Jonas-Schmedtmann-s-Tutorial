91de8fd233e795f7a3eca4b63ad21e7341e997cb
PaolaVlsc <paolavlsc170698@gmail.com>
2024-02-10 05:13:03 +0200

    adding key

--- 12_HowReactWorks/how-react-works/src/App.js
+++ 12_HowReactWorks/how-react-works/src/App.js
@@ -43,7 +43,10 @@ function Tabbed({ content }) {
         <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
       </div>
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
