174dd33aee4bdf9f27748affbd9db6395119dcba
PaolaVlsc <paolavlsc170698@gmail.com>
2024-02-10 10:24:23 +0200

    go back to App

--- 10_Popcorn/usepopcorn/src/index.js
+++ 10_Popcorn/usepopcorn/src/index.js
@@ -1,34 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
-import StarRating from "./StarRating";
-import { useState } from "react";
+// import StarRating from "./StarRating";
+// import { useState } from "react";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
+    <App />
+    {/* <StarRating maxRating={5} />
     <StarRating maxRating={10} />
     <StarRating />
     <StarRating size={24} color="red" className="test" />
     <StarRating
       messages={["Terrible", "Bad", "Good", "Very Good", "Excellent"]}
     />
-    <StarRating size={24} color="green" defaultRating={3} />
+    <StarRating size={24} color="green" defaultRating={3} /> */}
 
-    <Test />
+    {/* <Test /> */}
   </React.StrictMode>
 );
