16f52cbbaa9e80563da9957e90d62c3a6f156dfa
PaolaVlsc <paolavlsc170698@gmail.com>
2024-02-10 10:21:40 +0200

    fix prop types import

--- 10_Popcorn/usepopcorn/src/StarRating.js
+++ 10_Popcorn/usepopcorn/src/StarRating.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { PropTypes } from "react";
+import { PropTypes } from "prop-types";
 // We place it outside of the function so that it will not have to be regenreated by
 // JavaScript every time the function is called / re-rendered
 // This is a performance optimization technique
